2ead29d4d0e3e1cd0ee981210ffa929052ffd47c
efemirkan <efemirkanguner@gmail.com>
2026-02-18 13:12:30 +0000

    basic_stat file created and Basic dataset stats, Sentence length distributions and sanity check funtions added.

---
+++ src/basic_stats.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def dataset_stats(sents, tags):
+
+    num_sentences = len(sents)  # number of the sentences
+    num_tokens = sum(len(token) for token in sents) # number of the tokens
+    avg_sentence_length = num_tokens / num_sentences # Average length of the sentences
+    num_tags = len(tags) # number of the tags
+
+    print(f"Number of sentences: {num_sentences}")
+    print(f"Number of tokens: {num_tokens}")
+    print(f"Number of tags: {num_tags}")
+    print(f"Average sentence length: {avg_sentence_length}")
+
+def sent_length_distribution(sents):
+
+    length = [len(sent) for sent in sents]
+
+    print(f"Median length: {np.median(length)}")
+    print(f"95th percentile: {np.percentile(length, 95)}")
+    print(f"Max length: {np.max(length)}")
+
+def check_sanity(sents, tags):
+
+    for s, t in zip(sents, tags):
+        assert len(s) == len(t), f"Mismatch in {s} and {t}"
+
+    print(f"Tokens and tags matched")
